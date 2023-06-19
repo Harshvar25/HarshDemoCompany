ed98948e0930389865b765e9d261e33a53d7e9a1
Harshvar25 <harshvardhansingh.bais2021@vitstudent.ac.in>
2023-06-19 16:01:31 +0530

    Demo Company first git push

---
+++ src/pages/demo-company.js
@@ -0,0 +1,200 @@
+import "./demo-company.css";
+const DemoCompany = () => {
+  return (
+    <div className="democompany">
+      <div className="democompany-child" />
+      <a href="#"><img className="democompany-item" alt="" src="/group-34.svg" /></a> 
+      <a href=""><img className="democompany-inner" alt="" src="/group-38.svg" /></a> 
+      <div className="group-div">
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <div className="find-courses-parent">
+            <div className="find-courses"><input type="search" name="" value="" style={{backgroundColor:"#282c38",border:"none"}} placeholder="Seach cources"/></div>
+            <img
+              className="vuesaxlinearsearch-normal-icon"
+              alt=""
+              src="/vuesaxlinearsearchnormal.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="we-tackle-interesting">
+        We tackle interesting topics every day
+      </div>
+      <img className="group-icon" alt="" src="/group-27.svg" />
+      <img className="democompany-child1" alt="" src="/group-28.svg" />
+      <div className="democompany-inner1">
+        <div className="home-wrapper">
+          <div className="home" style={{cursor:"pointer"}}>Home</div>
+        </div>
+      </div>
+      <div className="work"style={{cursor:"pointer"}}>Work</div>
+      <div className="pricing"style={{cursor:"pointer"}}>Pricing</div>
+      <div className="blog"style={{cursor:"pointer"}}>Blog</div>
+      <div className="about"style={{cursor:"pointer"}}>About</div>
+      <img className="ellipse-icon" alt="" src="/ellipse-1@2x.png" style={{cursor:"pointer"}} />
+      <img className="vector-icon" alt="" src="/vector-134.svg"style={{cursor:"pointer"}} />
+      <div className="button">
+       <a href="#"><div className="all">All</div></a> 
+      </div>
+      <div className="button1">
+      <a href="#"><div className="all" style={{color:"#a5a5a8"}}>Technology</div></a> 
+      </div>
+      <a href="#"><div className="button2">
+        <div className="all">Lifestyle</div>
+      </div></a> 
+      <a href="#"><div className="button3">
+        <div className="all">Sport</div>
+      </div></a> 
+      <a href="#"><div className="button4">
+        <div className="all">Education</div>
+      </div></a> 
+      <a href="#"><div className="button5">
+        <div className="all">Healthcare</div>
+      </div></a> 
+      <a href=""><div className="button6">
+        <div className="all">Food</div>
+      </div></a> 
+      <div className="rectangle-group">
+        <div className="group-item" />
+        <img className="group-inner" alt="" src="/rectangle-1073@2x.png" />
+        <div className="getting-started">
+          Changing people's lifestyles for the better
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">LIFESTYLE</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+          <a href=""><img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="/vuesaxlineararrowright.svg"
+          /></a> 
+        </div>
+      </div>
+      <div className="rectangle-container">
+      
+        <div className="getting-started">
+          Changing people's lifestyles for the better
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">LIFESTYLE</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="/vuesaxlineararrowright1.svg"
+          />
+        </div>
+      </div>
+      <div className="rectangle-parent1">
+        <div className="group-item" />
+        <img className="group-inner" alt="" src="/rectangle-1075@2x.png" />
+        <div className="getting-started">
+          Simple Solutions for Complex Connections
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">TECHNOLOGY</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+          
+        </div>
+      </div>
+      <div className="rectangle-parent2">
+       
+        <div className="getting-started">
+          Simple Solutions for Complex Connections
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">TECHNOLOGY</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="/vuesaxlineararrowright3.svg"
+          />
+        </div>
+      </div>
+      <div className="rectangle-parent3">
+        <div className="group-item" />
+        <img className="group-inner" alt="" src="/rectangle-1077@2x.png" />
+        <div className="getting-started">
+          All things are difficult before they are easy
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">SPORT</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="/vuesaxlineararrowright4.svg"
+          />
+        </div>
+      </div>
+      <div className="rectangle-parent4">
+       
+        <div className="getting-started">
+          All things are difficult before they are easy
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">SPORT</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="/vuesaxlineararrowright5.svg"
+          />
+        </div>
+      </div>
+      <div className="rectangle-parent5">
+        <div className="group-item" />
+        <img className="group-inner" alt="" src="/rectangle-1079@2x.png" />
+        <div className="getting-started">
+          How to Memorize everything you read?
+        </div>
+        <div className="first-steps-to">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </div>
+        <div className="lifestyle1">EDUCATION</div>
+        <div className="hours-ago">2 hours ago</div>
+        <div className="read-more-parent">
+          <div className="read-more">Read More</div>
+         
+        </div>
+      </div>
+      
+      <div className="demo-company">Demo Company</div>
+    </div>
+  );
+};
+
+export default DemoCompany;
